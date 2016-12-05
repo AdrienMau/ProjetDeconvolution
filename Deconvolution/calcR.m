1e01292cfaa0118d7191c3ad2a57034dcad6a9cc
Ochon <paul.letendre35@gmail.com>
2016-12-05 15:35:33 +0100

    Revert "Déconvolution"

--- Deconvolution/calcR.m
+++
@@ -1,9 +0,0 @@
-function [r,x,y,phi]=calcR(d)
-
-if ~rem(size(d,2),2) % this might be a x/y image
-    x=d(:,1:(end/2)); y=d(:,(end/2+1):end);
-    r=sqrt( x.^2 + y.^2 );
-    phi=atan2(y,x);
-else
-    r=d;
-end
